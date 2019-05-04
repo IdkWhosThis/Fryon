b15e59372b04faaa31fab3fa7339c1b5b7caeb97
JustWolf <39670563+justwolfii@users.noreply.github.com>
2019-05-04 15:58:55 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -7,10 +7,26 @@ const PREFIX = "f!";
 var args = message.content.substring(PREFIX.length).split(" ");
 let uicon = message.author.avatarURL;
 
-if (!message.guild.member(bot.user).hasPermission('MANAGE_MESSAGES')) return console.log("Fryon | "+ message.author.username + " provedl příkaz " + message.content + ", ale nemám práva na MENAGE_MESSAGES. Server: " + message.guild.name); message.channel.send(':no_entry Potřebuji práva `MANAGE_MESSAGES`. :no_entry:').catch(console.error);
-            if (!message.member.hasPermission("MANAGE_MESSAGES")) return console.log("Fryon | "+ message.author.username + " provedl příkaz " + message.content + ", ale nemá práva na MANAGE_MESSAGES. Server: " + message.guild.name); message.channel.send(":no_entry: Omlouvám se, nemáš práva `MANAGE_MESSAGES` pro tento příkaz :no_entry:");
-            if (isNaN(args[1])) return console.log("Fryon | "+ message.author.username + " provedl příkaz " + message.content + ", ale neuvedl počet zpráv, co mám smazat. Server: " + message.guild.name); message.channel.send(':warning: Musíš uvést počet zpráv, co mám smazat :warning:');
-            if (args[1] > 100) return console.log("Fryon | "+ message.author.username + " provedl příkaz " + message.content + " na serveru: " + message.guild.name); message.channel.send(':warning: Nelze smazat více jak 100 zpráv :warning:');
+if (!message.guild.member(bot.user).hasPermission('MANAGE_MESSAGES')) {
+            console.log("Fryon | "+ message.author.username + " provedl příkaz " + message.content + ", ale nemám práva na MENAGE_MESSAGES. Server: " + message.guild.name); 
+            message.channel.send(':no_entry Potřebuji práva `MANAGE_MESSAGES`. :no_entry:').catch(console.error);
+            return;
+            }
+            if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            console.log("Fryon | "+ message.author.username + " provedl příkaz " + message.content + ", ale nemá práva na MANAGE_MESSAGES. Server: " + message.guild.name); 
+            message.channel.send(":no_entry: Omlouvám se, nemáš práva `MANAGE_MESSAGES` pro tento příkaz :no_entry:");
+            return;
+            }
+            if (isNaN(args[1])) {
+            console.log("Fryon | "+ message.author.username + " provedl příkaz " + message.content + ", ale neuvedl počet zpráv, co mám smazat. Server: " + message.guild.name); 
+            message.channel.send(':warning: Musíš uvést počet zpráv, co mám smazat :warning:');
+            return;
+            }
+            if (args[1] > 100) {
+            console.log("Fryon | "+ message.author.username + " provedl příkaz " + message.content + " na serveru: " + message.guild.name); 
+            message.channel.send(':warning: Nelze smazat více jak 100 zpráv :warning:');
+            return;
+            }
             message.channel.bulkDelete(args[1]);
             var cleanEmbed = new Discord.RichEmbed()
             .setAuthor('Zprávy byly úspěšně smazány')
